84c8c2bdd2e953cdc59f3385fd02f29e404fec1e
Christian Rorvik <christian.rorvik@gmail.com>
2011-06-16 21:03:30 +0100

    crunch_concurrency - Removed commented out code

--- crunch_concurrency/source/meta_scheduler.cpp
+++ crunch_concurrency/source/meta_scheduler.cpp
@@ -168,48 +168,4 @@ void MetaScheduler::Run(IWaitable& until)
 {
 }
 
-/*
-void MetaScheduler::Run()
-{
-    typedef std::vector<ContextRunState> ContextList;
-
-    ContextList runStates;
-    {
-        const boost::mutex::scoped_lock lock(mSchedulersLock);
-
-        BOOST_FOREACH(const SchedulerPtr& scheduler, mSchedulers)
-        {
-            ContextRunState runState;
-            runState.context = &scheduler->GetContext();
-            runStates.push_back(runState);
-        }
-    }
-
-    // Check if scheduler master list has changed
-    // - Add / Remove schedulers as needed
-    // - Should probably also hold copy of scheduler ptr as long as we use context
-    for (;;)
-    {
-        for (ContextList::iterator it = runStates.begin();
-            it != runStates.end();)
-        {
-            ISchedulerContext& context = *it->context;
-            const ISchedulerContext::State state = context.RunOne();
-            if (state == ISchedulerContext::STATE_IDLE)
-            {
-                it = runStates.erase(it);
-                // idleRunStates.push_back(&context);
-                context.GetHasWorkCondition().AddWaiter(&*it);
-            }
-            else
-            {
-                ++it;
-            }
-        }
-
-        // If active list empty, go idle
-    }
-}
-}*/
-
 }}
