fb4987a94553ad30b6184496b8b7e538f0bd6fa9
Christian Rorvik <christian.rorvik@gmail.com>
2011-06-16 20:09:09 +0100

    crunch_base - Added stdint.hpp

---
+++ crunch_base/include/crunch/base/stdint.hpp
@@ -0,0 +1,40 @@
+#ifndef CRUNCH_BASE_STDINT_HPP
+#define CRUNCH_BASE_STDINT_HPP
+
+#include "crunch/base/platform.hpp"
+
+#if defined (CRUNCH_PLATFORM_LINUX)
+#   include <cstdint>
+#endif
+
+namespace Crunch {
+
+#if defined (CRUNCH_PLATFORM_LINUX)
+
+typedef std::int8_t int8;
+typedef std::uint8_t uint8;
+typedef std::int16_t int16;
+typedef std::uint16_t uint16;
+typedef std::int32_t int32;
+typedef std::uint32_t uint32;
+typedef std::int64_t int64;
+typedef std::uint64_t uint64;
+
+#elif defined (CRUNCH_PLATFORM_WIN32)
+
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef long int32;
+typedef unsigned long uint32;
+typedef __int64 int64;
+typedef unsigned __int64 int64;
+
+#else
+#   error "Unsupported platform"
+#endif
+
+}
+
+#endif
