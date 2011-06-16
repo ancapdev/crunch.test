723d739d2cc46fc3589d1239916770bae05b35e5
Christian Rorvik <christian.rorvik@gmail.com>
2011-06-16 20:51:51 +0100

    crunch_base - Windows build fix

--- crunch_base/include/crunch/base/stdint.hpp
+++ crunch_base/include/crunch/base/stdint.hpp
@@ -29,7 +29,7 @@ typedef unsigned short uint16;
 typedef long int32;
 typedef unsigned long uint32;
 typedef __int64 int64;
-typedef unsigned __int64 int64;
+typedef unsigned __int64 uint64;
 
 #else
 #   error "Unsupported platform"
