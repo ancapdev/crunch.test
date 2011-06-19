619ca9f343071a8584b9c29e90e5ee59418ccaeb
Christian Rorvik <christian.rorvik@gmail.com>
2011-06-19 21:16:53 +0100

    crunch_concurrency - Added missing memory ordering parameter to methods on Atomic

--- crunch_concurrency/include/crunch/concurrency/atomic.hpp
+++ crunch_concurrency/include/crunch/concurrency/atomic.hpp
@@ -151,67 +151,67 @@ template<typename T, typename W = typename Platform::AtomicWord<T>::Type>
 class AtomicIntegerBase : public AtomicBase<T, W>
 {
 public:
-    T Increment() volatile
+    T Increment(MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicIncrement(this->mData.bits);
+        result.bits = Platform::AtomicIncrement(this->mData.bits, ordering);
         return result.value;
     }
 
-    T Decrement() volatile
+    T Decrement(MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicDecrement(this->mData.bits);
+        result.bits = Platform::AtomicDecrement(this->mData.bits, ordering);
         return result.value;
     }
 
-    T Add(T value) volatile
+    T Add(T value, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter value_;
         value_.value = value;
 
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicAdd(this->mData.bits, value_.bits);
+        result.bits = Platform::AtomicAdd(this->mData.bits, value_.bits, ordering);
         return result.value;
     }
 
-    T Sub(T value) volatile
+    T Sub(T value, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter value_;
         value_.value = value;
 
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicSub(this->mData.bits, value_.bits);
+        result.bits = Platform::AtomicSub(this->mData.bits, value_.bits, ordering);
         return result.value;
     }
 
-    T And(T mask) volatile
+    T And(T mask, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter mask_;
         mask_.value = mask;
 
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicAnd(this->mData.bits, mask_.bits);
+        result.bits = Platform::AtomicAnd(this->mData.bits, mask_.bits, ordering);
         return result.value;
     }
 
-    T Or(T mask) volatile
+    T Or(T mask, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter mask_;
         mask_.value = mask;
 
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicOr(this->mData.bits, mask_.bits);
+        result.bits = Platform::AtomicOr(this->mData.bits, mask_.bits, ordering);
         return result.value;
     }
 
-    T Xor(T mask) volatile
+    T Xor(T mask, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T, W>::Converter mask_;
         mask_.value = mask;
 
         typename AtomicBase<T, W>::Converter result;
-        result.bits = Platform::AtomicXor(this->mData.bits, mask_.bits);
+        result.bits = Platform::AtomicXor(this->mData.bits, mask_.bits, ordering);
         return result.value;
     }
 };
@@ -220,39 +220,39 @@ template<typename T>
 class AtomicIntegerBase<T, T> : public AtomicBase<T, T>
 {
 public:
-    T Increment() volatile
+    T Increment(MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicIncrement(this->mData.bits);
+        return Platform::AtomicIncrement(this->mData.bits, ordering);
     }
 
-    T Decrement() volatile
+    T Decrement(MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicDecrement(this->mData.bits);
+        return Platform::AtomicDecrement(this->mData.bits, ordering);
     }
 
-    T Add(T value) volatile
+    T Add(T value, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicAdd(this->mData.bits, value);
+        return Platform::AtomicAdd(this->mData.bits, value, ordering);
     }
 
-    T Sub(T value) volatile
+    T Sub(T value, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicSub(this->mData.bits, value);
+        return Platform::AtomicSub(this->mData.bits, value, ordering);
     }
 
-    T And(T mask) volatile
+    T And(T mask, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicAnd(this->mData.bits, mask);
+        return Platform::AtomicAnd(this->mData.bits, mask, ordering);
     }
 
-    T Or(T mask) volatile
+    T Or(T mask, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicOr(this->mData.bits, mask);
+        return Platform::AtomicOr(this->mData.bits, mask, ordering);
     }
 
-    T Xor(T mask) volatile
+    T Xor(T mask, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Platform::AtomicXor(this->mData.bits, mask);
+        return Platform::AtomicXor(this->mData.bits, mask, ordering);
     }
 };
 
@@ -310,33 +310,33 @@ public:
         Store(value, ordering);
     }
 
-    T* Increment() volatile
+    T* Increment(MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Add(1);
+        return Add(1, ordering);
     }
 
-    T* Decrement() volatile
+    T* Decrement(MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
-        return Sub(1);
+        return Sub(1, ordering);
     }
 
-    T* Add(std::ptrdiff_t value) volatile
+    T* Add(std::ptrdiff_t value, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T*>::Converter value_;
         value_.value = value * sizeof(T);
 
         typename AtomicBase<T*>::Converter result;
-        result.bits = Platform::AtomicAdd(this->mData.bits, value_.bits);
+        result.bits = Platform::AtomicAdd(this->mData.bits, value_.bits, ordering);
         return result.value;
     }
 
-    T* Sub(std::ptrdiff_t value) volatile
+    T* Sub(std::ptrdiff_t value, MemoryOrder ordering = MEMORY_ORDER_SEQ_CST) volatile
     {
         typename AtomicBase<T*>::Converter value_;
         value_.value = value * sizeof(T);
 
         typename AtomicBase<T*>::Converter result;
-        result.bits = Platform::AtomicSub(this->mData.bits, value_.bits);
+        result.bits = Platform::AtomicSub(this->mData.bits, value_.bits, ordering);
         return result.value;
     }
 
