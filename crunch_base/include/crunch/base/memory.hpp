4227a45ce6b9300456052b3dd55a846eb36dc866
Christian Rorvik <christian.rorvik@gmail.com>
2011-06-20 18:31:34 +0100

    crunch_base - Added memory.hpp with aligned allocation and pointer arithmetic helpers

---
+++ crunch_base/include/crunch/base/memory.hpp
@@ -0,0 +1,64 @@
+#ifndef CRUNCH_BASE_MEMORY_HPP
+#define CRUNCH_BASE_MEMORY_HPP
+
+#include "crunch/base/platform.hpp"
+
+#include <cstddef>
+#include <malloc.h>
+
+namespace Crunch {
+
+#if defined (CRUNCH_PLATFORM_WIN32)
+
+inline void* MallocAligned(std::size_t bytes, std::size_t alignment)
+{
+    return ::_malloc_aligned(bytes, alignment);
+}
+
+inline void FreeAligned(void* allocation)
+{
+    ::_free_aligned(allocation);
+}
+
+#elif defined (CRUNCH_PLATFORM_LINUX)
+
+inline void* MallocAligned(std::size_t bytes, std::size_t alignment)
+{
+    return ::memalign(alignment, bytes);
+}
+
+inline void FreeAligned(void* allocation)
+{
+    ::free(allocation);
+}
+
+#else
+#   error "Unsupported platform"
+#endif
+
+
+
+inline void* AlignPointerUp(void* pointer, std::size_t alignment)
+{
+    std::size_t const a = alignment - 1;
+    return (void*)(((std::size_t)pointer + a) & a);
+}
+
+inline void* AlignPointerDown(void* pointer, std::size_t alignment)
+{
+    return (void*)((std::size_t)pointer & (alignment - 1));
+}
+
+inline bool IsPointerAligned(void* pointer, std::size_t alignment)
+{
+    return ((std::size_t)pointer & (alignment - 1)) == 0;
+}
+
+inline void* OffsetPointer(void* pointer, std::ptrdiff_t delta)
+{
+    return (void*)((std::size_t)pointer + delta);
+}
+
+}
+
+#endif
