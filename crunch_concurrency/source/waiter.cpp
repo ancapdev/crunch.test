921ca1c0cae9f0eaf6b6fd83f0c82ccd7106fd8e
Christian Rorvik <christian.rorvik@gmail.com>
2011-07-16 19:15:33 +0100

    crunch_concurrency - Tidy up Waiter allocation.

--- crunch_concurrency/source/waiter.cpp
+++ crunch_concurrency/source/waiter.cpp
@@ -3,69 +3,103 @@
 
 #include "crunch/concurrency/waiter.hpp"
 #include "crunch/concurrency/atomic.hpp"
+#include "crunch/concurrency/detail/system_mutex.hpp"
 #include "crunch/base/stdint.hpp"
 
+#include <algorithm>
 #include <cstdlib>
+#include <vector>
 
 namespace Crunch { namespace Concurrency {
 
 namespace
 {
+    class WaiterAllocator
+    {
+    public:
+        WaiterAllocator()
+            : mFreeList(0)
+        {}
+
+        ~WaiterAllocator()
+        {
+            std::for_each(mAllocations.begin(), mAllocations.end(), [] (void* allocation) { std::free(allocation); });
+        }
+
+        void* Allocate()
+        {
+            uint64 head = mFreeList.Load(MEMORY_ORDER_RELAXED);
+            for (;;)
+            {
+                Waiter* headPtr = GetPointer(head);
+                if (headPtr == nullptr)
+                {
+                    Detail::SystemMutex::ScopedLock lock(mAllocationsLock);
+                    void* allocation = std::malloc(sizeof(Waiter));
+                    mAllocations.push_back(allocation);
+                    return allocation;
+                }
+                else
+                {
+                    uint64 const newHead = SetPointer(head, headPtr->next) + ABA_ADDEND;
+                    if (mFreeList.CompareAndSwap(newHead, head))
+                        return headPtr;
+                }
+            }
+        }
+
+        void Free(void* allocation)
+        {
+            Waiter* node = reinterpret_cast<Waiter*>(allocation);
+            uint64 head = mFreeList.Load(MEMORY_ORDER_RELAXED);
+            for (;;)
+            {
+                node->next = GetPointer(head);
+                uint64 const newHead = SetPointer(head, node) + ABA_ADDEND;
+                if (mFreeList.CompareAndSwap(newHead, head))
+                    return;
+            }
+        }
+
+    private:
 #if (CRUNCH_PTR_SIZE == 4)
-    uint64 const ABA_ADDEND = 4ull << 32;
-    uint64 const PTR_MASK = 0xffffffffull;
+        static uint64 const ABA_ADDEND = 4ull << 32;
+        static uint64 const PTR_MASK = 0xffffffffull;
 #else
-    // Assume 48 bit address space
-    uint64 const ABA_ADDEND = 1ull << 48;
-    uint64 const PTR_MASK = ABA_ADDEND - 1;
+        // Assume 48 bit address space
+        static uint64 const ABA_ADDEND = 1ull << 48;
+        static uint64 const PTR_MASK = ABA_ADDEND - 1;
 #endif
 
-    Waiter* GetPointer(uint64 ptrAndState)
-    {
-        return reinterpret_cast<Waiter*>(ptrAndState & PTR_MASK);
-    }
+        Waiter* GetPointer(uint64 ptrAndState)
+        {
+            return reinterpret_cast<Waiter*>(ptrAndState & PTR_MASK);
+        }
 
-    uint64 SetPointer(uint64 ptrAndState, Waiter* ptr)
-    {
-        CRUNCH_ASSERT((reinterpret_cast<uint64>(ptr) & ~PTR_MASK) == 0);
-        return (ptrAndState & ~PTR_MASK) | reinterpret_cast<uint64>(ptr);
-    }
+        uint64 SetPointer(uint64 ptrAndState, Waiter* ptr)
+        {
+            CRUNCH_ASSERT((reinterpret_cast<uint64>(ptr) & ~PTR_MASK) == 0);
+            return (ptrAndState & ~PTR_MASK) | reinterpret_cast<uint64>(ptr);
+        }
 
-    Atomic<uint64> gWaiterFreeList;
+        Atomic<uint64> mFreeList;
+        Detail::SystemMutex mAllocationsLock;
+        std::vector<void*> mAllocations;
+    };
+
+    WaiterAllocator gWaiterAllocator;
 }
 
 CRUNCH_THREAD_LOCAL Waiter* Waiter::tWaiterFreeList = 0;
 
 void* Waiter::AllocateGlobal()
 {
-    uint64 head = gWaiterFreeList.Load(MEMORY_ORDER_RELAXED);
-    for (;;)
-    {
-        Waiter* headPtr = GetPointer(head);
-        if (headPtr == nullptr)
-        {
-            return std::malloc(sizeof(Waiter));
-        }
-        else
-        {
-            uint64 const newHead = SetPointer(head, headPtr->next) + ABA_ADDEND;
-            if (gWaiterFreeList.CompareAndSwap(newHead, head))
-                return headPtr;
-        }
-    }
+    return gWaiterAllocator.Allocate();
 }
 
 void Waiter::FreeGlobal(void* allocation)
 {
-    Waiter* node = reinterpret_cast<Waiter*>(allocation);
-    uint64 head = gWaiterFreeList.Load(MEMORY_ORDER_RELAXED);
-    for (;;)
-    {
-        node->next = GetPointer(head);
-        uint64 const newHead = SetPointer(head, node) + ABA_ADDEND;
-        if (gWaiterFreeList.CompareAndSwap(newHead, head))
-            return;
-    }
+    return gWaiterAllocator.Free(allocation);
 }
 
 }}
