783f560b75ca73a807b71c73f2361473e98f4212
Christian Rorvik <christian.rorvik@gmail.com>
2011-07-17 15:27:27 +0100

    crunch_containers - Added FixedVector range and fill assignment tests.

--- crunch_containers/test/fixed_vector_tests.cpp
+++ crunch_containers/test/fixed_vector_tests.cpp
@@ -111,29 +111,38 @@ BOOST_AUTO_TEST_CASE(MoveAssignTest)
     BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount);
 }
 
-BOOST_AUTO_TEST_CASE(PushPopTest)
-{
-    Tracker::Statistics stats;
-    FixedVector<Tracker, 1> v;
-    v.push_back(Tracker(stats));
-    BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount + 1);
-    BOOST_CHECK(!v.empty());
-    BOOST_CHECK_EQUAL(v.size(), 1u);
-    BOOST_CHECK_EQUAL(v.end(), v.begin() + 1);
-    BOOST_CHECK_THROW(v.push_back(Tracker(stats)), std::length_error);
-    BOOST_CHECK_EQUAL(v.size(), 1u);
-    v.pop_back();
-    BOOST_CHECK(v.empty());
-    BOOST_CHECK_EQUAL(v.size(), 0u);
-    BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount);
-}
-
 BOOST_AUTO_TEST_CASE(RangeAssignTest)
 {
+    Tracker::Statistics stats;
+    {
+        Tracker trackers[3] =
+        {
+            Tracker(stats, 1),
+            Tracker(stats, 2),
+            Tracker(stats, 3)
+        };
+        FixedVector<Tracker, 8> v;
+        v.assign(trackers, trackers + 3);
+        BOOST_CHECK_EQUAL(v.size(), 3u);
+        BOOST_CHECK_EQUAL(v[0].GetTag(), 1);
+        BOOST_CHECK_EQUAL(v[1].GetTag(), 2);
+        BOOST_CHECK_EQUAL(v[2].GetTag(), 3);
+    }
+    BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount);
 }
 
 BOOST_AUTO_TEST_CASE(FillAssignTest)
 {
+    Tracker::Statistics stats;
+    {
+        FixedVector<Tracker, 8> v;
+        v.assign(3, Tracker(stats, 123));
+        BOOST_CHECK_EQUAL(v.size(), 3u);
+        BOOST_CHECK_EQUAL(v[0].GetTag(), 123);
+        BOOST_CHECK_EQUAL(v[1].GetTag(), 123);
+        BOOST_CHECK_EQUAL(v[2].GetTag(), 123);
+    }
+    BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount);
 }
 
 BOOST_AUTO_TEST_CASE(RangeInsertTest)
@@ -158,6 +167,23 @@ BOOST_AUTO_TEST_CASE(RangeEraseTest)
 
 BOOST_AUTO_TEST_CASE(ClearTest)
 {
+}
+
+BOOST_AUTO_TEST_CASE(PushPopTest)
+{
+    Tracker::Statistics stats;
+    FixedVector<Tracker, 1> v;
+    v.push_back(Tracker(stats));
+    BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount + 1);
+    BOOST_CHECK(!v.empty());
+    BOOST_CHECK_EQUAL(v.size(), 1u);
+    BOOST_CHECK_EQUAL(v.end(), v.begin() + 1);
+    BOOST_CHECK_THROW(v.push_back(Tracker(stats)), std::length_error);
+    BOOST_CHECK_EQUAL(v.size(), 1u);
+    v.pop_back();
+    BOOST_CHECK(v.empty());
+    BOOST_CHECK_EQUAL(v.size(), 0u);
+    BOOST_CHECK_EQUAL(stats.constructCount, stats.destructCount);
 }
 
 BOOST_AUTO_TEST_CASE(AccessTest)
