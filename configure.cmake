f565d3a2001bd49cc6dd6162164cf47efd7ea2c5
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-12 20:26:02 +0000

    Converted to package format

---
+++ configure.cmake
@@ -0,0 +1,4 @@
+# Copyright (c) 2011, Christian Rorvik
+# Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
+
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
---
+++ include/crunch/test/framework.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2011, Christian Rorvik
+// Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
+
+#ifndef CRUNCH_TEST_FRAMEWORK_HPP
+#define CRUNCH_TEST_FRAMEWORK_HPP
+
+#if defined (_MSC_VER)
+#   pragma warning (push)
+#   pragma warning (disable : 6326)
+#endif
+
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test_suite.hpp>
+
+#if defined (_MSC_VER)
+#   pragma warning (pop)
+#endif
+
+#endif
