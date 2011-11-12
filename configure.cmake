21de3ba0c8bb3c2b08ee159201ba27b10f28e6c3
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-12 21:37:42 +0000

    Fixes after vpm_framework change

--- configure.cmake
+++ configure.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2011, Christian Rorvik
 # Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
 
-vpm_add_dependencies(boost)
+vpm_depend(boost)
 
 add_definitions(-DBOOST_TEST_ALTERNATIVE_INIT_API)
 
