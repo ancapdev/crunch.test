ad96ecf805bd5e970ea40bb141aa550f84162b97
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-12 21:33:05 +0000

    Fixed configuration

--- configure.cmake
+++ configure.cmake
@@ -1,4 +1,17 @@
 # Copyright (c) 2011, Christian Rorvik
 # Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
 
+vpm_add_dependencies(boost)
+
+add_definitions(-DBOOST_TEST_ALTERNATIVE_INIT_API)
+
 include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
+
+macro(crunch_add_test _name)
+  add_custom_target("run-${_name}"
+    ALL
+    ${_name} "--log_level=message"
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    VERBATIM)
+endmacro()
+
