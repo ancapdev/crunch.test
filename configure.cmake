702a10de7a27063634571efaac4a83727015aa25
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-20 22:57:09 +0000

    Added custom targets for building and running all tests

--- configure.cmake
+++ configure.cmake
@@ -8,10 +8,25 @@ add_definitions(-DBOOST_TEST_ALTERNATIVE_INIT_API)
 vpm_include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 
 macro(crunch_add_test _name)
+  # Shared build target for all tests
+  if(NOT TARGET build-tests)
+    add_custom_target(build-tests)
+  endif()
+
+  # Shared run target for all tests
+  if(NOT TARGET run-tests)
+    add_custom_target(run-tests)
+  endif()
+
+  # Run target for this test
   add_custom_target("run-${_name}"
     ALL
     ${_name} "--log_level=message"
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     VERBATIM)
+
+  # Add dependencies on shared build and run targets
+  add_dependencies(build-tests ${_name})
+  add_dependencies(run-tests "run-${_name}")
 endmacro()
 
