63df620867ed65205f6dc10d911e960b0d310e13
Christian Rorvik <christian.rorvik@gmail.com>
2011-12-07 20:49:00 +0000

    Added missing source file

---
+++ source/dummy.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2011, Christian Rorvik
+// Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
+
+// Dummy source file to keep CMake happy for empty library
