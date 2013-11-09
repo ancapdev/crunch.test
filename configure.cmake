b14df76aa23b55e06a92836d25b562b56e3c2ab9
Christian Rorvik <christian.rorvik@gmail.com>
2013-11-09 22:04:56 +0000

    Split crunch_add_test to crunch_add_test_executable and crunch_add_test_targets for better control in dependents

--- configure.cmake
+++ configure.cmake
@@ -13,7 +13,7 @@ if(NOT Boost_USE_STATIC_LIBS)
   add_definitions("-DBOOST_TEST_DYN_LINK")
 endif()
 
-macro(crunch_add_test _name)
+macro(crunch_add_test_executable _name)
   # Add test executable, with main function defined by the boost unit testing framework
   add_executable(${_name}
     ${ARGN}
@@ -21,8 +21,10 @@ macro(crunch_add_test _name)
 
   target_link_libraries(${_name}
     boost_unit_test_framework)
+endmacro()
 
-  # Shared build target for all tests
+macro(crunch_add_test_targets _name)
+    # Shared build target for all tests
   if(NOT TARGET build-tests)
     add_custom_target(build-tests)
   endif()
@@ -42,5 +44,10 @@ macro(crunch_add_test _name)
   # Add dependencies on shared build and run targets
   add_dependencies(build-tests ${_name})
   add_dependencies(run-tests "run-${_name}")
+endmacro()
+
+macro(crunch_add_test _name)
+  crunch_add_test_executable(${_name} ${ARGN})
+  crunch_add_test_targets(${_name})
 endmacro()
 
